7c176d2aefca3cd9686bcff5d6e761ea0e36cff8
blackholegalaxy <blackholegalaxy@users.noreply.github.com>
2022-06-14 09:03:31 -0400

    fix test

--- src/app/core/services/window-ref/window-ref.service.spec.ts
+++ src/app/core/services/window-ref/window-ref.service.spec.ts
@@ -1,5 +1,5 @@
-import { InjectionToken } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 
 import { WindowRefService } from '@core/services';
@@ -12,11 +12,17 @@ describe('WindowRefService', () => {
     doc: jest.fn(),
   }) as any);
 
+  let angularFireAuth = jest.fn().mockReturnValue(() => ({
+    signInWithEmailAndPassword: jest.fn(),
+    signInWithPopup: jest.fn(),
+  }) as any);
+
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [
         WindowRefService,
         { provide: AngularFirestore, useValue: angularFirestore },
+        { provide: AngularFireAuth, useValue: angularFireAuth },
       ],
     });
 
--- src/app/core/services/window-ref/window-ref.service.ts
+++ src/app/core/services/window-ref/window-ref.service.ts
@@ -1,14 +1,39 @@
 import { Injectable } from '@angular/core';
-
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/compat/firestore';
 
-import firebase from 'firebase/compat';
+import firebase from 'firebase/compat/app';
+import '@firebase/auth';
 
 declare let window;
 
+import UserCredential = firebase.auth.UserCredential;
+
+const facebookAuthProvider = new firebase.auth.FacebookAuthProvider();
+const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
+const appleAuthProvider = new firebase.auth.OAuthProvider('apple.com');
+const twitterAuthProvider = new firebase.auth.TwitterAuthProvider();
+const githubAuthProvider = new firebase.auth.GithubAuthProvider();
+const microsoftAuthProvider = new firebase.auth.OAuthProvider('microsoft.com');
+const yahooAuthProvider = new firebase.auth.OAuthProvider('yahoo.com');
+
+enum AuthProvider {
+  ALL = 'all',
+  EmailAndPassword = 'firebase',
+  Google = 'google',
+  Apple = 'apple',
+  Facebook = 'facebook',
+  Twitter = 'twitter',
+  Github = 'github',
+  Microsoft = 'microsoft',
+}
+
 @Injectable()
 export class WindowRefService {
-  constructor(public angularFirestore: AngularFirestore) { }
+  constructor(
+    public angularFirestore: AngularFirestore,
+    public angularFireAuth: AngularFireAuth,
+  ) { }
 
   public get nativeWindow(): Window {
     return window;
@@ -86,6 +111,56 @@ export class WindowRefService {
       }
     } catch (e) {
       console.error('error while updating user data', e);
+    }
+  }
+
+  public async signInWith(provider: AuthProvider, credentials?: { email: string; password: string; }) {
+    try {
+      let signInResult: UserCredential | any;
+
+      switch (provider) {
+        case AuthProvider.EmailAndPassword:
+          signInResult = await this.angularFireAuth
+            .signInWithEmailAndPassword(credentials.email, credentials.password) as UserCredential;
+          break;
+
+        case AuthProvider.Google:
+          signInResult = await this.angularFireAuth
+            .signInWithPopup(googleAuthProvider) as UserCredential;
+          break;
+
+        case AuthProvider.Apple:
+          signInResult = await this.angularFireAuth
+            .signInWithPopup(appleAuthProvider) as UserCredential;
+          break;
+
+        case AuthProvider.Facebook:
+          signInResult = await this.angularFireAuth
+            .signInWithPopup(facebookAuthProvider) as UserCredential;
+          break;
+
+        case AuthProvider.Twitter:
+          signInResult = await this.angularFireAuth
+            .signInWithPopup(twitterAuthProvider) as UserCredential;
+          break;
+
+        case AuthProvider.Github:
+          signInResult = await this.angularFireAuth
+            .signInWithPopup(githubAuthProvider) as UserCredential;
+          break;
+
+        case AuthProvider.Microsoft:
+          signInResult = await this.angularFireAuth
+            .signInWithPopup(microsoftAuthProvider) as UserCredential;
+          break;
+
+        default:
+          throw new Error(`${AuthProvider[provider]} is not available as auth provider`);
+      }
+
+      console.log(signInResult);
+    } catch (err) {
+      console.error('signin', err);
     }
   }
 }
